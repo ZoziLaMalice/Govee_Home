e25c6f844cc432ce5893dfef8a60f0a50b0aacf3
ZoziLaMalice <lorenzo.camus@essca.eu>
2021-01-06 21:41:45 +0100

    First Commit

---
+++ GoveeAPI.py
@@ -0,0 +1,9 @@
+import requests
+
+headers = {"Govee-API-Key": "cbdd1086-bd4b-47c2-9b47-39717902f2ed"}
+r = requests.get('https://developer-api.govee.com/v1/devices', headers=headers)
+print(r.text)
+
+headers = {"Govee-API-Key": "cbdd1086-bd4b-47c2-9b47-39717902f2ed",
+           "Content-Type": "application/json"}
+r = requests.get('https://developer-api.govee.com/v1/devices/control', headers=headers)
