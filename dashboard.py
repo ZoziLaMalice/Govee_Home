f028bb260e3a07899bad930eac1cacd127f6d5c4
ZoziLaMalice <lorenzo.camus@essca.eu>
2021-01-12 18:22:41 +0100

    Cleaning repo

--- dashboard.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
-from flask_colorpicker import colorpicker
-app = Flask(__name__)
-Bootstrap(app)
-colorpicker(app)
--- index.py
+++ index.py
@@ -1,21 +1,5 @@
 from flask import Flask, render_template, redirect, request
-
-import re
 # from govee_btled import BluetoothLED
-
-# from flask_colorpicker import colorpicker
-# Some junk to solve loading module path from parent dir
-from sys import path
-from os import getcwd, name
-splitter = '\\' if name == 'nt' else '/'
-path.append(
-    splitter.join(
-        getcwd().split(
-            splitter
-        )[:-1]
-    )
-)
-# End of junk
 from flask_colorpicker import colorpicker
 
 app = Flask(__name__, template_folder='.')
