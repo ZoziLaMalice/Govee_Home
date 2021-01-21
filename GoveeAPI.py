bf83683b23cdc5eeb7dbea935775c1f45f430cfc
ZoziLaMalice <lorenzo.camus@essca.eu>
2021-01-21 01:04:46 +0100

    Update Python script on Govee API

--- GoveeAPI.py
+++ GoveeAPI.py
@@ -1,10 +1,34 @@
 import requests
 
-headers = {"Govee-API-Key": "cbdd1086-bd4b-47c2-9b47-39717902f2ed"}
-r = requests.get('https://developer-api.govee.com/v1/devices', headers=headers)
-print(r.text)
+def get_info():
+    headers = {"Govee-API-Key": "cbdd1086-bd4b-47c2-9b47-39717902f2ed"}
+    r = requests.get('https://developer-api.govee.com/v1/devices',
+                    headers=headers)
+    print(r.text)
+    return r
 
-headers = {"Govee-API-Key": "cbdd1086-bd4b-47c2-9b47-39717902f2ed",
-           "Content-Type": "application/json"}
-r = requests.get('https://developer-api.govee.com/v1/devices/control', headers=headers)
-print(r.text)
+def cmd(name, value):
+    headers = {"Govee-API-Key": "cbdd1086-bd4b-47c2-9b47-39717902f2ed",
+            "Content-Type": "application/json"}
+    params = {"device": "BD:B7:A4:C1:38:E6:78:B2",
+            "model": "H6159",
+            "cmd": {
+                "name": name,
+                "value": value
+            }}
+    r = requests.put('https://developer-api.govee.com/v1/devices/control', json=params, headers=headers)
+    print(r.text)
+    return r
+
+def get_state():
+    headers = {"Govee-API-Key": "cbdd1086-bd4b-47c2-9b47-39717902f2ed",
+            "Content-Type": "application/json"}
+    params = {"device": "BD:B7:A4:C1:38:E6:78:B2",
+            "model": "H6159"}
+    r = requests.get('https://developer-api.govee.com/v1/devices/state?',
+                    params=params, headers=headers)
+    print(r.text)
+    return r
+
+
+
